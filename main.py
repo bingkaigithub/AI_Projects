8a3faee1cc2e76b3e45180602c4ca1df8d4e5f87
bingkai <bingkai@ducoconsulting.ca>
2024-02-18 12:42:14 -0500

    init

---
+++ main.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from dotenv import load_dotenv
+import os
+from llama_index.core.query_engine import PandasQueryEngine
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from llama_index.core.agent import ReActAgent
+from llama_index.llms.openai import OpenAI
+
+from prompts import new_prompt, instruction_str, context
+from note_engine import note_engine
+from pdf import canada_engine
+
+load_dotenv()
+
+population_path = os.path.join("data", "population.csv")
+population_df = pd.read_csv(population_path)
+
+population_query_engine = PandasQueryEngine(
+    df=population_df, verbose=True, instruction_str=instruction_str)
+population_query_engine.update_prompts({"pandas_prompt": new_prompt})
+population_query_engine.query("what is the population of canada")
+
+tools = [
+    note_engine,
+    QueryEngineTool(
+        query_engine=population_query_engine, metadata=ToolMetadata(
+            name="population_data",
+            description="this gives information at the world population and demographics"
+        )
+    ),
+    QueryEngineTool(
+        query_engine=canada_engine, metadata=ToolMetadata(
+            name="canada_data",
+            description="this gives detailed information about canada"
+        )
+    )
+]
+
+llm = OpenAI(model="gpt-3.5-turbo-0613")
+agent = ReActAgent.from_tools(tools, llm=llm, verbose=True, context=context)
+
+while (prompt := input("Enter a promt (q to quit): ")) != "q":
+    result = agent.query(prompt)
+    print(result)
